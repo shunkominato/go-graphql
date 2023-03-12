17631e4d9a1523001f5dfd69d72347c68b60b3ad
shunkominato <shun19871001@gmail.com>
2023-03-12 10:31:26 +0900

    s

--- internal/infrastructure/server/graph/resolver/resolver.go
+++ internal/infrastructure/server/graph/resolver/resolver.go
@@ -1,7 +1,14 @@
 package graph
 
+import "story.com/story/app/internal/infrastructure/server/graph/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+
+type Resolver struct{
+	todos []*model.Todo
+	todoStatus []*model.TodoStatus
+	user *model.User
+}
